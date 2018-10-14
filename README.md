b987d32adc0c0f328035279aca2303f1bbf877a6
freewind <nowindlee@gmail.com>
2018-10-14 14:46:46 +0800

    demo

--- README.md
+++ README.md
@@ -1,19 +1,4 @@
-TypeScript tsconfig.json Demo
-============================
+TypeScript Exclude Type Demo
+===========================
 
-`tsconfig.json`是typescript项目的说明文件，指定`tsc`所需要的各个参数。
-
-```
-npm install
-npm run tsc
-```
-
-或者使用`ts-node`:
-
-```
-npm run demo
-```
-
-Resources
----------
-- tsconfig详解：<https://www.typescriptlang.org/docs/handbook/tsconfig-json.html>
+如何从一个类型中去掉部分类型
--- hello.ts
+++ hello.ts
@@ -1,3 +1,24 @@
-import {capitalize} from 'lodash';
+type Common = {
+  id: string,
+  createdAt: number;
+}
 
-console.log("Hello, " + capitalize("typescript") + "!");
+type Article = Common & {
+  title: string,
+  content: string
+}
+
+type MyExclude<T, U> = T extends U ? never : T;
+type MyPick<T, K extends keyof T> = {
+  [P in K]: T[P];
+};
+
+type ExcludeType<F, P> = MyPick<F, MyExclude<keyof F, keyof P>>
+
+type PartialArticle = ExcludeType<Article, Common>
+
+const partialArticle: PartialArticle = {
+  id: 'id',
+  title: 'hello',
+  content: 'typescript',
+};
